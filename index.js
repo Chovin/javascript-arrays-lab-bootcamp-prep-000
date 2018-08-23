2087f1b0e8d36cb3f5e6ed50fe082e096a3aed11
Chovin Carlson <chovin@guam.net>
2018-08-23 02:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  return [...kittens]
+}
+
+function prependKitten(name) {
+  return [...kittens]
+}
+
+function removeLastKitten() {
+  return [...kittens]
+}
+
+function removeFirstKitten() {
+  return [...kittens]
+}
